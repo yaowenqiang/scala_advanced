31a6f39bd0d92f21a3db7f15682620ddf1f33304
姚文强 <yaowenqiang111@163.com>
2022-10-15 18:19:13 +0800

    ad dead lock and live lock demo

--- src/lectures/part3Concurrency/ThreadCommunication.scala
+++ src/lectures/part3Concurrency/ThreadCommunication.scala
@@ -25,9 +25,9 @@ object ThreadCommunication extends App {
 
     def naiveProdCons(): Unit = {
         val container = new SimpleContainer
-        val consumer =  new Thread(() => {
+        val consumer = new Thread(() => {
             println("consumer waiting")
-            while(container.isEmpty) {
+            while (container.isEmpty) {
                 println("[consumer] actively waiting...")
             }
             println(s"[consumer] I have consumed ${container.get}")
@@ -46,17 +46,17 @@ object ThreadCommunication extends App {
 
     }
 
-//    naiveProdCons()
+    //    naiveProdCons()
 
     // wait and notify
 
 
-    def smartProducerConsumer() : Unit = {
+    def smartProducerConsumer(): Unit = {
         val container = new SimpleContainer
 
         val consumer = new Thread(() => {
             println("[consumer] waiting")
-            container.synchronized{
+            container.synchronized {
                 container.wait()
             }
 
@@ -68,7 +68,7 @@ object ThreadCommunication extends App {
             print(s"[producer] hard at work...")
             Thread.sleep(2000)
             val value = 42
-            container.synchronized{
+            container.synchronized {
                 println(s"[producr] I'm producing ${value}")
                 container.set(value)
                 container.notify()
@@ -79,27 +79,27 @@ object ThreadCommunication extends App {
         producer.start()
     }
 
-//    smartProducerConsumer()
+    //    smartProducerConsumer()
 
 
-    def prodConsLargeBuffer() : Unit = {
-        val buffer : mutable.Queue[Int] = new mutable.Queue[Int]
+    def prodConsLargeBuffer(): Unit = {
+        val buffer: mutable.Queue[Int] = new mutable.Queue[Int]
         val capacity = 3
 
         val consumer = new Thread(() => {
             val random = new Random()
-            while(true) {
-               buffer.synchronized {
-                   if(buffer.isEmpty) {
-                       println("[consumer] buffer empty ,waiting...")
-                       buffer.wait()
-                   }
+            while (true) {
+                buffer.synchronized {
+                    if (buffer.isEmpty) {
+                        println("[consumer] buffer empty ,waiting...")
+                        buffer.wait()
+                    }
 
-                   val x = buffer.dequeue()
-                   println(s"[consumer] consumed ${x} from buffer")
+                    val x = buffer.dequeue()
+                    println(s"[consumer] consumed ${x} from buffer")
 
-                   buffer.notify()
-               }
+                    buffer.notify()
+                }
                 Thread.sleep(random.nextInt(500))
             }
         })
@@ -107,9 +107,9 @@ object ThreadCommunication extends App {
         val producer = new Thread(() => {
             val random = new Random()
             var i = 0
-            while(true) {
+            while (true) {
                 buffer.synchronized {
-                    if(buffer.size == capacity) {
+                    if (buffer.size == capacity) {
                         println("[producer] buffer is full, waiting...")
                         buffer.wait()
                     }
@@ -128,15 +128,15 @@ object ThreadCommunication extends App {
         producer.start()
     }
 
-//    prodConsLargeBuffer()
+    //    prodConsLargeBuffer()
 
 
     class Consumer(id: Int, buffer: mutable.Queue[Int]) extends Thread {
         override def run(): Unit = {
             val random = new Random()
-            while(true) {
+            while (true) {
                 buffer.synchronized {
-                    while(buffer.isEmpty) {
+                    while (buffer.isEmpty) {
                         println(s"[consumer ${id}] buffer empty ,waiting...")
                         buffer.wait()
                     }
@@ -152,13 +152,13 @@ object ThreadCommunication extends App {
     }
 
 
-    class Producer(id: Int, buffer: mutable.Queue[Int], capacity :Int) extends Thread {
+    class Producer(id: Int, buffer: mutable.Queue[Int], capacity: Int) extends Thread {
         override def run(): Unit = {
             val random = new Random()
             var i = 0
-            while(true) {
+            while (true) {
                 buffer.synchronized {
-                    while(buffer.size == capacity) {
+                    while (buffer.size == capacity) {
                         println(s"[producer ${id}] buffer is full, waiting...")
                         buffer.wait()
                     }
@@ -175,12 +175,81 @@ object ThreadCommunication extends App {
         }
     }
 
-    def multiProdCons(nConsumers: Int, nProducers: Int) :Unit = {
-        val buffer : mutable.Queue[Int] = new mutable.Queue[Int]
+    def multiProdCons(nConsumers: Int, nProducers: Int): Unit = {
+        val buffer: mutable.Queue[Int] = new mutable.Queue[Int]
         val capacity = 3
         (1 to nConsumers).foreach(i => new Consumer(i, buffer).start())
         (1 to nProducers).foreach(i => new Producer(i, buffer, capacity).start())
     }
 
-    multiProdCons(3, 3)
+    //    multiProdCons(3, 3)
+
+
+    // notify all
+
+    def testNotifyAll(): Unit = {
+        val bell = new Object
+        (1 to 10).foreach(i => new Thread(() => {
+            bell.synchronized {
+                println(s"[thread ${i} waiting ...")
+                bell.wait()
+                println(s"[thread ${i} hooray!")
+            }
+        }).start())
+
+        new Thread(() => {
+            Thread.sleep(2000)
+            println("Announcer Rock's roll!")
+            bell.synchronized {
+                bell.notifyAll()
+                //                bell.notify()
+            }
+        }).start()
+    }
+
+    //    testNotifyAll()
+    // dead lock
+    case class Friend(name: String) {
+        def bow(other: Friend): Unit = {
+            this.synchronized {
+                println(s"${this} : I am bowing to my friend ${other}")
+                other.bow(this)
+                println(s"${this} : my friend ${other} has risen")
+            }
+        }
+
+        def rise(other: Friend): Unit = {
+            this.synchronized {
+                println(s"${this} : I am rising to my friend ${other}")
+            }
+        }
+
+        var side = "right"
+
+        def switchSide(): Unit = {
+            if (side == "right") side = "left"
+            else side = "right"
+        }
+
+        def pass(other: Friend): Unit = {
+            while (this.side == other.side) {
+                println(s"${this}: Oh, but please, ${other}, feel free to pass...")
+                switchSide()
+                Thread.sleep(1000)
+            }
+        }
+
+
+    }
+
+    val sam = Friend("Sam")
+    val pierre = Friend("Pierre")
+//    new Thread(() => sam.bow(pierre)).start() // sam's lock, then pierre.lock
+//    new Thread(() => pierre.bow(sam)).start() // pierre.lock, then sam's lock
+    // live lock
+
+        new Thread(() => sam.pass(pierre)).start() // sam's lock, then pierre.lock
+        new Thread(() => pierre.pass(sam)).start() // pierre.lock, then sam's lock
+
+
 }
