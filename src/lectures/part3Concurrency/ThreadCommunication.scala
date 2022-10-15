f441de5cdf2a1da0bf36a12fa94ce33d32d3f5f8
姚文强 <yaowenqiang111@163.com>
2022-10-15 16:43:36 +0800

    add buffer to producer and consumer

--- src/lectures/part3Concurrency/ThreadCommunication.scala
+++ src/lectures/part3Concurrency/ThreadCommunication.scala
@@ -1,5 +1,8 @@
 package lectures.part3Concurrency
 
+import scala.collection.mutable
+import scala.util.Random
+
 object ThreadCommunication extends App {
     /*
     the producer-consumer problem
@@ -76,7 +79,55 @@ object ThreadCommunication extends App {
         producer.start()
     }
 
-    smartProducerConsumer()
+//    smartProducerConsumer()
 
+
+    def prodConsLargeBuffer() : Unit = {
+        val buffer : mutable.Queue[Int] = new mutable.Queue[Int]
+        val capacity = 3
+
+        val consumer = new Thread(() => {
+            val random = new Random()
+            while(true) {
+               buffer.synchronized {
+                   if(buffer.isEmpty) {
+                       println("[consumer] buffer empty ,waiting...")
+                       buffer.wait()
+                   }
+
+                   val x = buffer.dequeue()
+                   println(s"[consumer] consumed ${x} from buffer")
+
+                   buffer.notify()
+               }
+                Thread.sleep(random.nextInt(500))
+            }
+        })
+
+        val producer = new Thread(() => {
+            val random = new Random()
+            var i = 0
+            while(true) {
+                buffer.synchronized {
+                    if(buffer.size == capacity) {
+                        println("[consumer] buffer is full, waiting...")
+                        buffer.wait()
+                    }
+
+                    println(s"[producer] producing ${i} into buffer")
+                    buffer.enqueue(i)
+
+                    buffer.notify()
+                    i += 1
+
+                }
+                Thread.sleep(random.nextInt(500))
+            }
+        })
+        consumer.start()
+        producer.start()
+    }
+
+    prodConsLargeBuffer()
 
 }
