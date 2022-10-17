4747f5a4c81b327f2964913376a11b7f925f1e1f
姚文强 <yaowenqiang111@163.com>
2022-10-17 22:36:04 +0800

    add variant exercises

--- src/lectures/part5TypeSystem/Variance.scala
+++ src/lectures/part5TypeSystem/Variance.scala
@@ -96,4 +96,52 @@ object Variance extends App {
     - return type are in covariant position
      */
 
+//    exercises
+
+    class Vehicle
+    class Bike extends Vehicle
+    class Car extends Vehicle
+
+    class IParking[T](vehicles: List[T]) {
+        def park[T](vehicle: Vehicle): IParking[T] = ???
+        def impound(vehicles: List[Vehicle]) : IParking[T] = ???
+        def checkVehicles(conditions: String) : List[T] = ???
+        def flatMap[T](f: T => IParking[T]) : IParking[T] = ???
+    }
+    class CParking[+T](vehicles: List[T]) {
+        def park[S >: T](vehicle: S): CParking[S] = ???
+        def impound[S >: T](vehicles: List[S]) : CParking[S] = ???
+        def checkVehicles(conditions: String) : List[T] = ???
+        def flatMap[S](f: T => CParking[S]) : IParking[S] = ???
+    }
+    class XParking[-T](vehicles: List[T]) {
+        def park(vehicle: T): XParking[T] = ???
+        def impound(vehicles: List[T]) : XParking[T] = ???
+        def checkVehicles[S <: T](conditions: String) : List[S] = ???
+//        def flatMap[S](f: T => XParking[S]) : XParking[S] = ???
+        // TODO
+        def flatMap[R <:T, S](f: T => XParking[R]) : XParking[S] = ???
+    }
+    class IList[T]
+
+    /*
+    rule of thumb:
+    - use covariance = collection of things
+    - use contravariance = group or actions
+     */
+
+
+    class CParking2[+T](vehicles: IList[T]) {
+        def park[S >: T](vehicle: S): CParking[S] = ???
+        def impound[S >: T](vehicles: IList[S]) : CParking2[S] = ???
+        def checkVehicles[S >: T](conditions: String) : IList[S] = ???
+    }
+    class XParking2[-T](vehicles: IList[T]) {
+        def park(vehicle: T): XParking2[T] = ???
+        def impound[S <: T](vehicles: IList[S]) : XParking2[S] = ???
+        def checkVehicles[S <: T](conditions: String) : IList[S] = ???
+    }
+
+
+
 }
