1d7d5fe80433c77955e2873ec7210b1ac74a6c80
姚文强 <yaowenqiang111@163.com>
2022-10-19 23:32:33 +0800

    add path dependent types demo

---
+++ src/lectures/part5TypeSystem/PathDependentTypes.scala
@@ -0,0 +1,54 @@
+package lectures.part5TypeSystem
+
+import scala.math.Numeric.IntIsIntegral
+
+object PathDependentTypes extends App {
+    class Outer {
+        class Inner
+        object InnerObject
+        type InnerType
+        def print(i : Inner) = println(i)
+        def printGeneral(i : Outer#Inner) = println(i)
+
+    }
+    def aMethod: Int = {
+        class HelperClass
+//        type HelperType
+        type HelperType = String
+        42
+    }
+    // per instance
+    val o = new Outer
+    val inner = new o.Inner //is a type
+    val oo = new Outer
+//    val otherInner :oo.Inner = new o.Inner // won't work
+
+    o.print(inner)
+    o.printGeneral(inner)
+    oo.printGeneral(inner)
+//    oo.print(inner)
+
+    // path dependent type
+
+    trait ItemLike {
+        type key
+    }
+    trait Item[K] extends ItemLike {
+        type key = K
+    }
+    trait IntItem extends Item[Int]
+    trait StringItem extends Item[String]
+
+//    def get[ItemType <: ItemLike](key: ItemType#key): ItemType
+    def get[ItemType <: ItemLike](key: ItemType#key): ItemType
+    get[IntItem](42)
+    get[StringItem]("Scala")
+//    get[IntItem]("Scala")
+    // TODO
+
+
+
+
+
+
+}
